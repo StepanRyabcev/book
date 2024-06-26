218b4788a050edceeb3ff457c2f1b5bb06ab3cfd
StepanRyabcev <stepanrabcev6@gmail.com>
2024-06-26 14:46:54 +0300

    Creates book object

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QString>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -12,3 +14,12 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_submit_clicked()
+{
+    QString name = ui->b_name->toPlainText();
+    QString genre = ui->b_genre->toPlainText();
+    int str = ui->b_str->value();
+    book = new Book(name, genre, str);
+}
+
